a148d7ac57789800e4ee7ddee993d224044c071e
andy02081224 <andy02081224@gmail.com>
2016-10-20 00:07:20 +0800

    Update Panel component
    Add textAlign property

--- client/js/components/Panel/Panel.jsx
+++ client/js/components/Panel/Panel.jsx
@@ -5,13 +5,23 @@ const Panel = function(props) {
 	let header = props.header 
 		? <div className="panel__header">{props.header}</div> 
 		: null;
+	let style = { textAlign: props.textAlign }
 
 	return (
-		<div className="panel">
+		<div className="panel" style={style}>
 			{header}
 			<div className="panel__body">{props.children}</div>
 		</div>
 	);
+};
+
+Panel.propTypes = {
+	header: React.PropTypes.string,
+	textAlign: React.PropTypes.string
+};
+
+Panel.defaultProps = {
+	textAlign: 'center'
 };
 
 export default Panel;
